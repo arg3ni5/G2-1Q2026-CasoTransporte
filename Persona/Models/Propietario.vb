8b42d59462c0a8c6288615059b0486c0765076ba
TATIM08 <aslymurillo080204@gmail.com>
2026-03-16 11:51:48 -0600

    Agrega clase Propietario y actualiza a .NET 4.8
    Se incorpora la clase Propietario al proyecto Persona, incluyendo sus propiedades y constructores. Se actualiza el framework objetivo a .NET 4.8 en los proyectos Persona y Transporte. Se ajustan los archivos Web.config para reflejar el nuevo framework y se mejora el formato. Además, se elimina la carpeta database\ del proyecto Transporte.

---
+++ Persona/Models/Propietario.vb
@@ -0,0 +1,42 @@
+﻿Public Class Propietario
+    Private _IdPersona As Integer
+    Private _Telefono As String
+    Private _Direccion As String
+
+    'constructor vacio 
+    Public Sub New()
+    End Sub
+
+    'constructor completo
+    Public Sub New(IdPersona As Integer, Telefono As String, Direccion As String)
+        Me.IdPersona = IdPersona
+        Me.Telefono = Telefono
+        Me.Direccion = Direccion
+
+     Public Property IdPersona As String
+        Get
+            Return _IdPersona
+        End Get
+        Set(value As String)
+            _IdPersona = value
+        End Set
+    End Property
+
+    Public Property Telefono As String
+        Get
+            Return _Telefono
+        End Get
+        Set(value As String)
+            _Telefono = value
+        End Set
+    End Property
+
+    Public Property Direccion As String
+        Get
+            Return _Direccion
+        End Get
+        Set(value As String)
+            _Direccion = value
+        End Set
+    End Property
+End Class

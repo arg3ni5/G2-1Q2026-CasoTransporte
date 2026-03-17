710202a94fed04bd1536c5b86dcc33cc2f6f6a1d
Dylan Rodríguez Bogantes <drodriguezb@edu.uc.ac.cr>
2026-03-16 21:36:23 -0600

    Añadir página Usuarios y clase modelo Usuario
    Se agrega la página Usuarios.aspx con validadores y GridView para gestión de usuarios. Se incorpora la clase Models.Usuario con propiedades básicas

---
+++ Transporte/Usuarios.aspx.vb
@@ -0,0 +1,22 @@
+﻿Public Class Usuarios
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs)
+
+        Dim username As String = TxtUsername.Text
+        Dim password As String = TxtPassword.Text
+        Dim rol As String = DdlRol.SelectedValue
+
+
+
+
+
+    End Sub
+
+
+
+End Class
